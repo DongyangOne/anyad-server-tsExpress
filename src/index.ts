a763035df7c3ff49b8ad261b8183c4cd7788340c
pino._.hoo <inhoo23@naver.com>
2022-09-22 17:16:59 +0900

    Create Project

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+const app = express()
+const port = 8000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(
+  cors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  }),
+)
+
+app.listen(port, () => console.log(`SERVER ON! PORT : ${port}`))
