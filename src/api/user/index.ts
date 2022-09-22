32d8f446e0a1d8cfed61856cf3d5687569d74b2a
pino._.hoo <inhoo23@naver.com>
2022-09-22 23:08:19 +0900

    Send Mail 기능 추가

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -2,6 +2,8 @@ const router = require("express").Router()
 const user = require("./user.service")
 
 router.post("/", user.localSave)
+router.post("/local", user.localLogin)
+router.post("/sendmail", user.sendMail)
 
 module.exports = router
 export {}
--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express"
+import passport from "passport"
 import { User } from "../../models/domain/User"
+import jwt from "jsonwebtoken"
 import { Provider } from "../../models/interface/Provider"
+import nodemailer from "nodemailer"
 
 exports.localSave = async (req: Request, res: Response) => {
   try {
@@ -15,4 +18,61 @@ exports.localSave = async (req: Request, res: Response) => {
   } catch (err) {
     console.log(err)
   }
+}
+
+exports.localLogin = async (req: Request, res: Response) => {
+  exports.localLogin = async (req: Request, res: Response) => {
+    passport.authenticate("local", { session: false }, (err, user) => {
+      if (err || !user) {
+        return res.status(400).json({
+          message: "Something is not right",
+          user: user,
+        })
+      }
+      req.login(user, { session: false }, (err) => {
+        if (err) {
+          res.send(err)
+        }
+        const token = jwt.sign({ idx: user.idx }, "123")
+        res.cookie("accessToken", token, {
+          expires: new Date(Date.now() + 86400e3),
+          sameSite: "lax",
+        })
+        return res.json({ user, token })
+      })
+    })(req, res)
+  }
+}
+
+exports.sendMail = async (req: Request, res: Response) => {
+  try {
+    const checkCode = String(getNumber())
+    const transporter = nodemailer.createTransport({
+      service: "Naver",
+      port: 587,
+      host: "smtp.gmail.com",
+      secure: false,
+      requireTLS: false,
+      auth: {
+        user: "inhoo25@naver.com",
+        pass: "wjd1127@!",
+      },
+    })
+    const mailOptions = {
+      from: "inhoo25@naver.com",
+      to: req.body.email,
+      subject: "[ANYAD Sign Up Check Code]",
+      text: `Your Code : ${checkCode}`,
+    }
+    await transporter.sendMail(mailOptions)
+    res.json({ code: checkCode })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const getNumber = () => {
+  let number = Math.floor(Math.random() * 1000000) + 100000
+  if (number > 1000000) number -= 100000
+  return number
 }
