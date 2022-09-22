aa00e169a938a0638b925a87579cb09912bb0a81
pino._.hoo <inhoo23@naver.com>
2022-09-22 17:44:56 +0900

    Swagger 셋팅

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import sequelize from './models'
+import path from 'path'
+import YAML from 'yamljs'
+import swaggerUi from 'swagger-ui-express'
 
 const app = express()
 const port = 8000
 
+const swaggerSpec = YAML.load(
+  path.join(__dirname, './utils/swagger/openapi.yaml'),
+)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
@@ -17,7 +24,16 @@ app.use(
     credentials: true,
   }),
 )
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`SERVER ON! PORT : ${port}`)
+  await sequelize
+    .authenticate()
+    .then(async () => {
+      console.log('connection success')
+    })
+    .catch((e) => {
+      console.log(e)
+    })
 })
